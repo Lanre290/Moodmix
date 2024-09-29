858325516884d11600a2686ecfea69da4c38e09e
Ashiru Olanrewaju <144792314+Lanre290@users.noreply.github.com>
2024-09-29 23:17:14 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -245,16 +245,15 @@ onClick={() => {
               </button>
             </a>
 
-          /*//  /*<button
-              //className="bg-blue-500 cursor-pointer 
-//px-6 //py-2 rounded-xl text-gray-50 hover:bg-blue-600
-//mx-auto"
-             // onClick={() => {
-           //     setShowPlaylistDiv(false);
-             // }}
-           // >
-             // Dismiss
-           // </button>*/
+          /*<button
+              className="bg-blue-500 cursor-pointer px-6 py-2 rounded-xl text-gray-50 hover:bg-blue-600
+mx-auto"
+             onClick={() => {
+              setShowPlaylistDiv(false);
+              }}
+            >
+             Dismiss
+           </button>*/
           </div>
         </div>
       )}
