10010e442e1d3f99a76e5cad6879cc598c732371
Ashiru Olanrewaju <lanre2967@gmail.com>
2024-09-30 21:17:16 +0100

    ui fix

--- src/App.tsx
+++ src/App.tsx
@@ -243,7 +243,6 @@ const App = () => {
                 Open playlist
               </button>
             </a>
-              
               className="bg-blue-500 cursor-pointer px-6 py-2 rounded-xl text-gray-50 hover:bg-blue-600 mx-auto"
               onClick={() => {
                 setShowPlaylistDiv(false);
